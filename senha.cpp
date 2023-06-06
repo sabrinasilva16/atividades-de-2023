02f4fcd4357c61add9fe935fbbce33eb0be1b25a
sabrinasilva16 <124587247+sabrinasilva16@users.noreply.github.com>
2023-06-06 15:08:40 -0300

    Add files via upload

---
+++ senha.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+
+int main(void){
+	
+	int senha;
+
+
+    printf(" Digite a sua senha: ");
+    scanf("%d",&senha);
+    
+	if (senha == 1234)      
+    printf(" Acesso permitido");
+          
+	else
+    printf(" Acesso negado"); 
+			 
+ }          
